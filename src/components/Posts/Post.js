c3587a8f64422c08b4eaee7df05445c066879615
mcampbell34 <mcampbell@fastly.com@fastly.com>
2020-12-29 19:25:58 -0800

    Somewhat Done, working out the bugs

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Comments from '../Comments/Comments';
 import LikeSection from './LikeSection';
 import PostHeader from './PostHeader';
+import Posts from './Posts';
 
 const Post = props => {
   // 🔥 Make sure the parent of Post is passing the right props!
@@ -22,8 +23,10 @@ const Post = props => {
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
       <LikeSection likePost={() => likePost(post.id)} />
-      {/* Comments also wants its props! */}
-      <Comments />
+      <LikeSection numberofLikes={Posts.likes} post={post} likePost={() => likePost(post.id)} /> 
+    
+
+      <Comments post={post} commments={Posts.Comments} />
     </div>
   );
 };
